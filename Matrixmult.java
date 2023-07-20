12b31f18d433f3dd783c9b25802baa62cc315600
MPMAHITHSAI <mpmahithsai@gmail.com>
2023-07-20 11:54:36 +0530

    matrix 2d product

---
+++ Matrixmult.java
@@ -0,0 +1,35 @@
+package Matrixproduct;
+
+import java.util.*;
+
+public class Matrixmult {
+	
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[][]=new int[n][n];
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		int b=sc.nextInt();
+		int values[][]=matrixproduct(arr,n,b);
+		for(int[] value:values) {
+			System.out.println(Arrays.toString(value));
+		}
+	}
+static int[][] matrixproduct(int arr[][],int n,int b){
+	
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<n;j++) {
+			arr[i][j]=arr[i][j]*b;
+		}
+	
+	}
+	return arr;
+}
+
+}
